991e61bfee6855e282d7256c7e82838f8f438f9b
Universalprogrammer <brunnostudios164@gmail.com>
2025-09-07 22:49:24 -0300

    Update block3.js

--- RED BULLS 25/block3.js
+++ RED BULLS 25/block3.js
@@ -3,6 +3,21 @@ audio.addEventListener("click", function() {
     audio.play()
 });
 
+function send() {
+  let file = document.getElementById('Audio').files[0];
+  if (!file) return alert("No seleccionaste ningún archivo.");
+  if (!file.type.startsWith("audio/")) return alert("Solo se permiten archivos de audio.");
+
+  let reader = new FileReader();
+  reader.onload = function(e) {
+    let base64Audio = e.target.result;
+    localStorage.setItem("customAudio", base64Audio);
+    localStorage.setItem("customAudioName", file.name); // nombre original
+    alert("Audio guardado correctamente 🎶");
+  };
+  reader.readAsDataURL(file);
+}
+
 
 function Notsend(){
     location.href = "Entrada.html";
